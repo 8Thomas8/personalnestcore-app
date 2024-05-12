02bdd95602e9867b9a0e8d11b549ffef3ceaeb5b
Thomas Rousseau <thomas.rousseau@proton.me>
2024-05-12 22:29:06 +0200

    Install(Theme) add material design v3

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,10 +2,11 @@ import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { md3 } from "vuetify/blueprints";
 
 export default defineNuxtPlugin((app) => {
     const vuetify = createVuetify({
-        // ... your configuration
+        blueprint: md3
     })
     app.vueApp.use(vuetify)
 })
