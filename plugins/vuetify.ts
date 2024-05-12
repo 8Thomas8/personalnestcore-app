95cc28d69afc7bd1d9394494603349278250393b
Thomas Rousseau <33552864+8Thomas8@users.noreply.github.com>
2024-05-12 22:48:06 +0200

    Install(lint) add eslint and prettier (#3)

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,11 +2,11 @@ import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import { md3 } from "vuetify/blueprints";
+import { md3 } from 'vuetify/blueprints'
 
-export default defineNuxtPlugin((app) => {
-    const vuetify = createVuetify({
-        blueprint: md3
-    })
-    app.vueApp.use(vuetify)
+export default defineNuxtPlugin(app => {
+  const vuetify = createVuetify({
+    blueprint: md3,
+  })
+  app.vueApp.use(vuetify)
 })
