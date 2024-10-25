47efbdc9c9104fbd9c7470d01cac242ff45a557b
Thomas Rousseau <33552864+8Thomas8@users.noreply.github.com>
2024-10-25 22:10:24 +0200

    Fix/clean history (#8)
    * Revert "feat(Profil) fix authentication and add profile page (#6)"
    
    This reverts commit d0ec5e8b90622ab5baa3a395f698bf3bf3d9f099.
    
    * Revert "feat(Authentication) user can login, logout, get user informations (#5)"
    
    This reverts commit ea14001e6b6b1726eb3194569c37fb5e86219767.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,3 @@
 <template>
-  <v-container>
-    <h1>Index page</h1>
-  </v-container>
+  <h1>Index page</h1>
 </template>
--- types/Token.ts
+++
@@ -1,8 +0,0 @@
-export type Token = {
-  abilities: string[]
-  expiresAt: string
-  lastUsedAt: string
-  name: string
-  token: string
-  type: 'bearer'
-}
