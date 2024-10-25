7a8d2b3539ae162d6ea36b48cd8721ac177d34dd
Thomas Rousseau <33552864+8Thomas8@users.noreply.github.com>
2024-10-25 22:10:24 +0200

    chore(Project) clean/revert history (#8)
    * Revert "feat(Profil) fix authentication and add profile page (#6)"
    
    * Revert "feat(Authentication) user can login, logout, get user informations (#5)"

--- app.vue
+++ app.vue
@@ -1,13 +1,3 @@
-<script lang="ts" setup>
-import { useUserStore } from '~/store/user'
-
-const { getData } = useUserStore()
-
-onBeforeMount(async () => {
-  await getData()
-})
-</script>
-
 <template>
   <NuxtLayout>
     <v-app>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,5 @@
-<script setup lang="ts">
-import AuthDialog from '~/components/dialogs/AuthDialog.vue'
-import Header from '~/components/commons/Header.vue'
-</script>
-
 <template>
   <div>
-    <Header />
     <slot />
-    <client-only>
-      <AuthDialog />
-    </client-only>
   </div>
 </template>
