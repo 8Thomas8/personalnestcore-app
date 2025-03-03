4e83ed47f810fc128153de34872ba0ab718af429
Thomas Rousseau <33552864+8Thomas8@users.noreply.github.com>
2025-03-03 01:14:00 +0100

    feat(Footer) add app version (#36)

--- components/commons/AppFooter.vue
+++ components/commons/AppFooter.vue
@@ -3,6 +3,7 @@
     <v-row justify="center" no-gutters>
       <v-col class="text-center mt-1" cols="12">
         {{ new Date().getFullYear() }} — <strong>personalnestcore</strong>
+        <CommonsAppVersion />
       </v-col>
     </v-row>
   </v-footer>
