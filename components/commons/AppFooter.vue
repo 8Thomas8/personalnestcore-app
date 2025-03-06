9474d9e94729ab60589fa6dd3e0b9bbc22aa3450
Thomas Rousseau <thomas.rousseau@proton.me>
2025-03-06 01:27:50 +0100

    feat(Footer) add report bug link

--- components/commons/AppFooter.vue
+++ components/commons/AppFooter.vue
@@ -1,17 +1,30 @@
 <template>
   <v-footer class="bg-grey-lighten-3">
     <v-row justify="center" no-gutters>
-      <v-col class="text-center mt-1" cols="12">
-        {{ new Date().getFullYear() }} — <strong>personalnestcore</strong>
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }}
+        <v-icon icon="mdi-copyright" size="18" />
+        <span class="font-weight-bold">PersonalNestCore</span>
       </v-col>
       <v-col class="text-center" cols="12"> <CommonsAppVersion /> | <CommonsApiVersion /> </v-col>
+      <v-col comls="12" class="text-center">
+        <v-btn
+          prepend-icon="mdi-bug"
+          append-icon="mdi-bug"
+          variant="text"
+          title="Signaler un bug ?"
+          href="https://github.com/personalnestcore/personalnestcore-issues"
+          target="_blank">
+          Signaler un bug ?
+        </v-btn>
+      </v-col>
     </v-row>
   </v-footer>
 </template>
 
 <style lang="scss" scoped>
 .v-footer {
-  max-height: 56px;
+  max-height: 100px;
 }
 </style>
 <script setup lang="ts"></script>
