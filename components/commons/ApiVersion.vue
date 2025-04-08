839cf472cab1aa3b0cd748aa96ec9d701c79abd4
Thomas Rousseau <33552864+8Thomas8@users.noreply.github.com>
2025-04-09 00:31:10 +0200

    refacto(Version) add cookie in order to prevent too many check (#66)

--- components/commons/ApiVersion.vue
+++ components/commons/ApiVersion.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { useAppStore } from '~/store/app'
+import { COOKIE } from '~/types/constants'
 
 const appStore = useAppStore()
+const cookie = useCookie(COOKIE.API_VERSION_CHECKED)
 
 onMounted(async () => {
+  if (cookie.value) return
   await appStore.fetchApiVersion()
   await appStore.fetchRepoApiVersion()
+  cookie.value = true
 })
 </script>
 
