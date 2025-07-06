64bf40987504a0fc7d4b16f0652fe7e446c23c4d
Thomas Rousseau <thomas.rousseau@proton.me>
2025-07-06 11:46:24 +0200

    fixup! feat(KitBuilder) add new feature

--- components/commons/pharmacy/KitBuilder.vue
+++ components/commons/pharmacy/KitBuilder.vue
@@ -167,7 +167,7 @@ const updateSelectedKit = async () => {
             <v-list>
               <v-list-item v-if="selectedKit.list.length === 0">
                 <v-list-item-content>
-                  <v-list-item-title>Aucun item dans ce kit</v-list-item-title>
+                  <v-list-item-title class="brea">Aucun item dans ce kit</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
               <v-list-item
